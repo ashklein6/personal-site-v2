425a55b81ad4ee96c99845b33ae511347daede75
ashklein6 <ashklein6@gmail.com>
2020-10-23 13:34:27 -0500

    updated about copy

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -12,7 +12,7 @@ const About = () => {
     <StyledAboutSection>
       <h2>About Me</h2>
       <p>Hey there! I'm Ashley, a full-stack software engineer based in Minneapolis, MN.</p>
-      <p>I love to solve problems by designing and coding beautifully simple things. I thrive in a fast-paced environment and am passionately in the pursuit of knowledge and optimization.</p>
+      <p>I'm logically minded and creative at heart. I love to solve problems by designing and coding beautifully simple things. I thrive in a fast-paced environment and am passionately in the pursuit of knowledge and optimization.</p>
       <p>After graduating from the {' '}<a href='https://www.wisc.edu/'>University of Wisconsin–Madison</a> {' '} I worked as a mechanical engineer in nuclear pharmaceuticals. I enjoyed troubleshooting our project's software so much that I attended {' '}<a href='https://primeacademy.io/'>Prime Academy</a> {' '} to transition to software development. I got my start in software working on developing the mobile application for {' '}<a href='https://www.trypikup.com/'>Pikup</a>.</p>
       <p>Here are a few technologies I've been working with recently:</p>
       <ul>
