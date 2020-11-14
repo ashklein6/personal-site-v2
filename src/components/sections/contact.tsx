8dddd673a9ec22a9b0b3a136a6adebfd6b40b028
ashklein6 <ashklein6@gmail.com>
2020-11-13 18:19:26 -0600

    added doodles and headshot

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -1,25 +1,54 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 import styled from 'styled-components'
 
 const StyledContactSection = styled.section`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
+  align-items: center;
+  justify-content: flex-end;
+  text-align: center;
 
   a {
     ${({ theme }) => theme.elements.button};
   }
+
+  .floral_3 {
+    top: -30px;
+    left: -10px;
+    width: calc(100% - 200px);
+    margin: 60px 0 30px;
+
+    @media (max-width: 768px) {
+      width: calc(100% - 50px)
+    }
+  }
 `
 
 const Contact = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      floral_3: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "floral_3.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   const email = 'ashklein6@gmail.com'
   return (
     <StyledContactSection>
       <h2>Let's Chat</h2>
       <p>I'm actively searching for my next position and would love to discuss opportunities.</p>
       <a href={`mailto:${email}`}>Say Hello</a>
+      <div className={'floral_3'}>
+        <Img fluid={data.floral_3.childImageSharp.fluid}/>
+      </div>
     </StyledContactSection>
   )
 }
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
 import styled from 'styled-components'
 
 const StyledHeroSection = styled.section`
@@ -30,9 +32,92 @@ const StyledHeroSection = styled.section`
   a {
     ${({ theme }) => theme.elements.button};
   }
+
+  .floral_2 {
+    position: absolute;
+    z-index: -5;
+    top: -30px;
+    left: -10px;
+    height: 400px;
+    width: 400px;
+
+    @media (max-width: 768px) {
+      width: 300px;
+      height: 300px;
+      left: -30px;
+    }
+  }
+`
+
+const StyledHeadshot = styled.div`
+  position: absolute;
+  right: 10%;
+  bottom: 30px;
+  height: 300px;
+  width: 300px;
+  z-index: -5;
+
+  @media (max-width: 768px) {
+    width: 200px;
+    height: 200px;
+  }
+
+  .headshot-shadow {
+    position: absolute;
+    height: 300px;
+    width: 300px;
+    margin: 10px -10px -10px 10px;
+    background-color: var(--pine);
+
+    @media (max-width: 768px) {
+      width: 200px;
+      height: 200px;
+    }
+  }
+
+  .floral_1 {
+    position: absolute;
+    bottom: -150px;
+    right: 100px;
+    height: 400px;
+    width: 400px;
+
+    @media (max-width: 768px) {
+      bottom: -100px;
+      right: 80px;
+      width: 250px;
+      height: 250px;
+    }
+  }
 `
 
 const Hero = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      headshot: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "headshot.jpg"}) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      floral_1: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "floral_1.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      floral_2: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "floral_2.png"}) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   const email = 'ashklein6@gmail.com'
   return (
     <StyledHeroSection>
@@ -41,6 +126,16 @@ const Hero = () => {
       <p>I code neat things for web and mobile.</p>
       <p>Based in Minneapolis.</p>
       <a href={`mailto:${email}`}>Say Hello</a>
+      <StyledHeadshot>
+        <div className={'headshot-shadow'}/>
+        <Img fluid={data.headshot.childImageSharp.fluid}/>
+        <div className={'floral_1'}>
+          <Img fluid={data.floral_1.childImageSharp.fluid}/>
+        </div>
+      </StyledHeadshot>
+      <div className={'floral_2'}>
+        <Img fluid={data.floral_2.childImageSharp.fluid}/>
+      </div>
     </StyledHeroSection>
   )
 }
