c0320cdc8a06813531c2127ff4fac7c2b6f8b92b
ashklein6 <ashklein6@gmail.com>
2020-11-17 09:34:48 -0600

    updated canonical link in helmet

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -7,7 +7,7 @@ export default function Head () {
       <html lang="en" />
       <meta charSet="utf-8" />
       <title>Ashley Klein</title>
-      <link rel="canonical" href="http://mysite.com/example" />
+      <link rel="canonical" href="https://ashleyklein.me" />
     </Helmet>
   )
 }
